68c849b75455df96eacc957748cb0373c9e2b6fb
Christian Wengert <christian.wengert@smartpowerpool.ch>
2017-04-24 14:15:52 +0200

    imap commands

--- bin/app.py
+++ bin/app.py
@@ -3,8 +3,9 @@ import argparse
 import curses
 import datetime
 import toolz
-
+import npyscreen
 from net.imap.imapclient import IMAPClient
+
 
 IMAP_SEARCH_FIELDS = {
     'ALL': str,
@@ -191,118 +192,6 @@ def parse_command_line_args():
     return parser.parse_args()
 
 
-#
-#
-# class DemoFrame(Frame):
-#     def __init__(self, screen):
-#         super(DemoFrame, self).__init__(screen,
-#                                         screen.height,
-#                                         screen.width,
-#                                         has_border=False,
-#                                         name="__mail__")
-#         # Internal state required for doing periodic updates
-#         self._last_frame = 0
-#         self._sort = 5
-#         self._reverse = True
-#
-#         # Create the basic form layout...
-#         layout = Layout([1], fill_frame=True)
-#         self._header = TextBox(1, as_string=True)
-#         self._header.disabled = True
-#         self._header.custom_colour = "label"
-#         self._list = MultiColumnListBox(
-#             Widget.FILL_FRAME,
-#             [">6", 10, ">4", ">7", ">7", "100%"],
-#             [],
-#             titles=["PID", "USER", "NI", "VIRT", "RSS", "CPU%"],
-#             name="mc_list")
-#         self._searchbox = Text(label='search', name=None, on_change=None, validator=None)
-#
-#         self.add_layout(layout)
-#         layout.add_widget(self._header)
-#         layout.add_widget(self._list)
-#         layout.add_widget(self._searchbox)
-#
-#         layout.add_widget(Label("Press `<`/`>` to change sort, `r` to toggle order, or `q` to quit."))
-#         self.fix()
-#
-#         # Add my own colour palette
-#         self.palette = defaultdict(
-#             lambda: (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK))
-#         for key in ["selected_focus_field", "label"]:
-#             self.palette[key] = (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK)
-#         self.palette["title"] = (Screen.COLOUR_BLACK, Screen.A_NORMAL, Screen.COLOUR_WHITE)
-#
-#     def process_event(self, event):
-#         # Do the key handling for this Frame.
-#         if isinstance(event, KeyboardEvent):
-#             if event.key_code in [ord('q'), ord('Q'), Screen.ctrl("c")]:
-#                 raise StopApplication("User quit")
-#             elif event.key_code in [ord("r"), ord("R")]:
-#                 self._reverse = not self._reverse
-#             elif event.key_code == ord("<"):
-#                 self._sort = max(0, self._sort - 1)
-#             elif event.key_code == ord(">"):
-#                 self._sort = min(7, self._sort + 1)
-#
-#             # Force a refresh for improved responsiveness
-#             self._last_frame = 0
-#
-#         # Now pass on to lower levels for normal handling of the event.
-#         return super(DemoFrame, self).process_event(event)
-#
-#     def _update(self, frame_no):
-#         self.b = 1
-#         # Refresh the list view if needed
-#         if frame_no - self._last_frame >= self.frame_update_count or self._last_frame == 0:
-#             self._last_frame = frame_no
-#
-#             # Create the data to go in the multi-column list...
-#             last_selection = self._list.value
-#             last_start = self._list.start_line
-#             list_data = []
-#             for a in range(3):
-#
-#
-#                 data = [
-#                         1 * a * self.b,
-#                         2 * a * self.b,
-#                         3 * a * self.b,
-#                         4 * a * self.b,
-#                         5 * a * self.b,
-#                         6 * a * self.b,
-#                     ]
-#                 list_data.append(data)
-#
-#             new_data = [
-#                 ([
-#                      str(x[0]),
-#                      str(x[1]),
-#                      str(x[2]),
-#                      str(x[3]),
-#                      str(x[4]),
-#                      str(x[5])
-#
-#                  ], x[0]) for x in list_data
-#             ]
-#
-#             # Update the list and try to reset the last selection.
-#             self._list.options = new_data
-#             self._list.value = last_selection
-#             self._list.start_line = last_start
-#             self._header.value = "fdfddf"
-#             self.b += 1
-#
-#         # Now redraw as normal
-#         super(DemoFrame, self)._update(frame_no)
-#
-#     # @property
-#     # def frame_update_count(self):
-#     #     Refresh once every 2 seconds by default.
-#         # return 100
-
-
-
 args = parse_command_line_args()
 print('Connecting...')
 # client = create_client_from_config(args)
@@ -328,48 +217,15 @@ x = server.fetch([141], data=['BODY[HEADER.FIELDS (SUBJECT FROM)]', 'BODY.PEEK[1
 
 # das hier mit attachment
 # http://stackoverflow.com/questions/6225763/downloading-multiple-attachments-using-imaplib
-x = server.fetch([139], data=['BODYSTRUCTURE'])
-f = x[139][b'BODYSTRUCTURE']
-len(f[0])
-#
-# while True:
-#     query = input('gimme')
-#     if query == 'quit' or query == 'q':
-#         break
-#     if query == 'help':
-#         print('help todo')
-#         continue
-#     terms = []
-#
-#     processed_query = [x.split(':') for x in query.split()]
-#
-#     grouped = toolz.groupby(lambda x: x[0], processed_query)
-#
-#     for group, items in grouped.items():
-#         if len(items) > 1:
-#             terms.append('OR')
-#
-#         for field, term in items:
-#             field = field.upper()
-#             if field.startswith('-'):
-#                 field = field[1:]
-#                 terms.append('NOT')
-#             convert = IMAP_SEARCH_FIELDS.get(field, None)
-#             if field in IMAP_SEARCH_FIELDS:
-#                 terms.append(field)
-#             if term and convert:
-#                 terms.append(convert(term))
-#
-#     print(terms)
-#     print(server.search(terms))
+# x = server.fetch([139], data=['BODYSTRUCTURE'])
+# f = x[139][b'BODYSTRUCTURE']
 
 
 def process_command(query):
-
     if query == 'quit' or query == 'q':
         pass
     if query == 'help':
-        pass
+        pass  # todo show all search options
 
     terms = []
 
@@ -391,14 +247,22 @@ def process_command(query):
                 terms.append(field)
             if term and convert:
                 terms.append(convert(term))
-    mails = server.search(terms)
-    return mails
+    mails = []
+    if terms:
+        try:
+            mails = server.search(terms)
+        except ValueError:  # no criteria specidfied
+            pass
+        except Exception as e:  # todo what else
+            pass
 
-import npyscreen
+    return mails
 
 
 def process_subject(data):
     return data.decode('utf8').replace('Subject: ', '').strip()
+
+
 def process_from(data):
     return data.decode('utf8').replace('From: ', '').strip()
 
@@ -408,11 +272,12 @@ def process_body(data):
 
 
 def process_size(data):
-    return int(data)
+    return int(data)  # todo make human readable
 
 
 def process_flags(data):
-    return data  # todo
+    return data  # todo split and show nicely
+
 
 class ActionControllerSearch(npyscreen.ActionControllerSimple):
     def create(self):
@@ -426,8 +291,9 @@ class ActionControllerSearch(npyscreen.ActionControllerSimple):
 
         self.parent.wCommand.value = ''
         ids = process_command(command_line)
-
-        mails = server.fetch(ids, data=['BODY[HEADER.FIELDS (SUBJECT)]', 'BODY[HEADER.FIELDS (FROM)]', 'BODY.PEEK[1] <0.100>', 'FLAGS', 'RFC822.SIZE'])
+        # todo has attachment?
+        mails = server.fetch(ids, data=['BODY[HEADER.FIELDS (SUBJECT)]', 'BODY[HEADER.FIELDS (FROM)]',
+                                        'BODY.PEEK[1] <0.100>', 'FLAGS', 'RFC822.SIZE'])
 
         # data = []
         # [data.append([k, b'BODY[HEADER.FIELDS (SUBJECT FROM)]', b'BODY[1][0]', 'FLAGS', 'RFC822.SIZE']) for k,v in mails.items()]
@@ -447,59 +313,60 @@ class ActionControllerSearch(npyscreen.ActionControllerSimple):
 
 class MyTextCommandBox(npyscreen.TextCommandBox):
     def __init__(self, screen,
-                history=True,
-                history_max=1000,  # todo make persistent
-                set_up_history_keys=True,
-                *args, **kwargs):
+                 history=True,
+                 history_max=1000,  # todo make persistent
+                 set_up_history_keys=True,
+                 *args, **kwargs):
         super(MyTextCommandBox, self).__init__(screen, history, history_max, set_up_history_keys, *args, **kwargs)
-
-def on_table_update(*args, **kwargs):
-    a = 3
 
 
 class MyGrid(npyscreen.GridColTitles):
     def __init__(self, screen, col_titles=None, *args, **kwargs):
-
         super(MyGrid, self).__init__(screen, col_titles, *args, **kwargs)
-        self.select_whole_line = True
-
-
-    def set_up_handlers(self):
-        super(MyGrid, self).set_up_handlers()
+        self.complex_handlers = []
         self.handlers = {
             curses.ascii.NL: self.select,
             curses.ascii.CR: self.select,
             curses.KEY_UP: self.h_move_line_up,
-                    curses.KEY_LEFT:    self.h_move_cell_left,
-                    curses.KEY_DOWN:    self.h_move_line_down,
-                    curses.KEY_RIGHT:   self.h_move_cell_right,
-                    "k":                self.h_move_line_up,
-                    "h":                self.h_move_cell_left,
-                    "j":                self.h_move_line_down,
-                    "l":                self.h_move_cell_right,
-                    curses.KEY_NPAGE:   self.h_move_page_down,
-                    curses.KEY_PPAGE:   self.h_move_page_up,
-                    curses.KEY_HOME:    self.h_show_beginning,
-                    curses.KEY_END:     self.h_show_end,
-                    ord('g'):           self.h_show_beginning,
-                    ord('G'):           self.h_show_end,
-                    curses.ascii.TAB:   self.h_exit,
+            curses.KEY_LEFT: self.h_move_cell_left,
+            curses.KEY_DOWN: self.h_move_line_down,
+            curses.KEY_RIGHT: self.h_move_cell_right,
+            curses.KEY_NPAGE: self.h_move_page_down,
+            curses.KEY_PPAGE: self.h_move_page_up,
+            curses.KEY_HOME: self.h_show_beginning,
+            curses.KEY_END: self.h_show_end,
+            ord('g'): self.h_show_beginning,
+            ord('G'): self.h_show_end,
+            curses.ascii.TAB: self.h_exit,
+            curses.KEY_BTAB: self.h_exit_up,
+            '^P': self.h_exit_up,
+            '^N': self.h_exit_down,
+            ord('q'): self.h_exit,
+            curses.ascii.ESC: self.h_exit,
+            curses.KEY_MOUSE: self.h_exit_mouse,
+            "r": self.reply,
+            "f": self.forward,
+            "d": self.delete,
+            "l": self.label,
+        }
+        # self.select_whole_line = True
 
-                    curses.KEY_BTAB:     self.h_exit_up,
-                    '^P':               self.h_exit_up,
-                    '^N':               self.h_exit_down,
-                    #curses.ascii.NL:    self.h_exit,
-                    #curses.ascii.SP:    self.h_exit,
-                    #ord('x'):       self.h_exit,
-                    ord('q'):       self.h_exit,
-                    curses.ascii.ESC:   self.h_exit,
-                    curses.KEY_MOUSE:    self.h_exit_mouse,
-                }
+    def set_up_handlers(self):
+        super(MyGrid, self).set_up_handlers()
 
-        self.complex_handlers = [
-                    ]
+    def reply(self, *args, **kwargs):
+        pass
+
+    def forward(self, *args, **kwargs):
+        pass
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    def label(self, *args, **kwargs):
+        pass
+
     def select(self, *args, **kwargs):
-
         a, b = self.edit_cell
         mail_id = self.values[a][0]
         # todo fetch message and siplay it on a separate screen
@@ -517,51 +384,48 @@ class FmSearchActive(npyscreen.FormMuttActive):
     COMMAND_WIDGET_BEGIN_ENTRY_AT = None
     COMMAND_ALLOW_OVERRIDE_BEGIN_ENTRY_AT = True
 
-    # DATA_CONTROLER = DataController
-    # new_handlers = {
-    #  curses.KEY_ENTER: on_table_update,
-    # }
-    MAIN_WIDGET_CLASS.select_whole_line = True
 
-    # a = MAIN_WIDGET_CLASS.event_handlers
-    bb  =3
-
-
-
-
-class TestApp(npyscreen.NPSApp):
+class Inbox(npyscreen.NPSApp):
     def main(self):
         F = FmSearchActive()
-        F.wStatus1.value = "mail "
-        F.wStatus2.value = "search / commands "
+        F.wStatus1.value = "mail/inbox "
+        F.wStatus2.value = "search "
 
         F.wMain.values = []
-        # for x in range(36):
-        #     row = []
-        #     for y in range(4):
-        #         row.append(y)
-        #     F.wMain.values.append(row)
 
         F.wMain.col_titles = ['id', 'from', 'subject', 'content', 'flags', 'size']
 
         F.edit()
 
 
+class Screens(npyscreen.NPSApp):  # shows which screens are available
+    def main(self):
+        pass
+
+
+class Rules(npyscreen.NPSApp):  # Set up rules
+    def main(self):
+        pass
+
+
+class ReadMessage(npyscreen.NPSApp):  # Read message
+    def main(self):
+        pass
+
+
+class WriteMessage(npyscreen.NPSApp):  # Write message
+    def main(self):
+        pass
+
+
+class Settings(npyscreen.NPSApp):  # Settings
+    def main(self):
+        pass
+
 if __name__ == "__main__":
-    App = TestApp()
+    App = Inbox()
     App.run()
 
 if __name__ == "__main__":
-    App = TestApp()
+    App = Inbox()
     App.run()
-    main()
-
-    # def demo(screen):
-    #     screen.play([Scene([DemoFrame(screen)], -1)], stop_on_resize=True)
-    #
-    # while True:
-    #     try:
-    #         Screen.wrapper(demo, catch_interrupt=True)
-    #         sys.exit(0)
-    #     except ResizeScreenError:
-    #         pass
